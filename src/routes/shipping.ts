5e5d2a662092cf7990659a467bb8ab897c1b6130
benparr64 <benparr64@outlook.com>
2023-02-25 15:32:40 +0000

    change to post

--- src/routes/shipping.ts
+++ src/routes/shipping.ts
@@ -3,8 +3,7 @@ import { Order } from "../models/shipping.types";
 import { getShippingRule } from "../services/shipping/shipping";
 
 export function shippingRoutes(app: express.Application): void {
-  // GET /products
-  app.get("/shipping", async (req: Request, res: Response) => {
+  app.post("/shipping", async (req: Request, res: Response) => {
     try {
       const { weight, price, postcode } = req.body as Order;
       const rule = getShippingRule(weight, price, postcode);
